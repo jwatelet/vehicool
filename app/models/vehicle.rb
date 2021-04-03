83fc31585d63825af490657a2da42287b667dc08
Johan Watelet <johanwatelet@gmail.com>
2021-04-03 21:24:54 +0200

    add hotwire

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -7,4 +7,8 @@ class Vehicle < ApplicationRecord
   validates :name, presence: true
   validates :brand_id, presence: true
   validates :department_id, presence: true
+
+  after_create_commit { broadcast_append_to 'vehicles' }
+  after_update_commit { broadcast_replace_to 'vehicles' }
+  after_destroy_commit { broadcast_remove_to 'vehicles' }
 end
