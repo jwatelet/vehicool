059529ceaafd615fda0b9a5a923ed5398f5bc604
Johan Watelet <johanwatelet@gmail.com>
2021-05-31 19:03:55 +0200

    config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "@fortawesome/fontawesome-free/css/fa-regular"
+import "@fortawesome/fontawesome-free/css/all"
 import $ from "jquery"
 
 global.$ = $
