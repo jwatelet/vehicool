38776fb026e7ff8318f3060d25af924c7af76ca8
Johan Watelet <johanwatelet@gmail.com>
2021-12-27 02:02:24 +0100

    add brand first tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "rspec/rails"
 require "devise"
 require "capybara/rails"
-require_relative "support/controller_macros"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -71,6 +70,5 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include FactoryBot::Syntax::Methods
-  config.extend ControllerMacros, :type => :controller
   config.include Warden::Test::Helpers
 end
